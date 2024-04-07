3e07eaf5de0dd06fde751cdb8067394b235703a7
Jossichi <32387226+jossichi@users.noreply.github.com>
2024-04-07 13:52:42 +0700

    menubar.js

--- javascript/menubar.js
+++ javascript/menubar.js
@@ -36,3 +36,13 @@ document.querySelector('ul#menu li:nth-child(3) a').addEventListener('click', fu
     document.getElementById('product4').style.display = 'none';
     document.getElementById('product5').style.display = 'none';
 });
+
+document.querySelector('ul#menu li:nth-child(4) a').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('product1').style.display = 'none';
+    document.getElementById('product2').style.display = 'none';
+    document.getElementById('banner').style.display = 'none';
+    document.getElementById('product3').style.display = 'none';
+    document.getElementById('product4').style.display = 'block';
+    document.getElementById('product5').style.display = 'none';
+});
